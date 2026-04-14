c1420ac3f8f629e7886a1a5af6004551a0f9d3bb
stephanoignacio28-del <stephanoignacio28@outlook.com>
2026-04-14 07:45:51 -0500

    Version final y funcional del ejercicio 1

--- ejercicio1_combate.py
+++ ejercicio1_combate.py
@@ -1,16 +1,16 @@
-def calcular_dano:
+def calcular_dano(ataque, defensa):
     dano = ataque - defensa
-    if dano < 0:
+    if (dano < 0):
         return 0
     return dano
-def aplicar_dano:
+def aplicar_dano(hp_actual, dano):
     nuevo_hp = hp_actual - dano
-    if nuevo_hp < 0:
+    if (nuevo_hp < 0):
         return 0
     return nuevo_hp
-def mostrar_estado:
+def mostrar_estado(nombre, hp, hp_max=100):
     print(f"{nombre}: {hp}/{hp_max}")
-def realizar_ataque:
+def realizar_ataque(atacante, defensor, dano):
     print(f"{atacante} ataca a {defensor} por {dano} de dano!")
 hp_heroe = 100
 hp_enemigo = 50
